7fa951d05ecf7199b2d7bde41c3997afbb7a3565
programmer1188 <madhusudhanareddy.2016@vitstudent.ac.in>
2022-03-25 14:52:37 -0500

    Initial commit

---
+++ app/src/main/java/com/example/instagram/Post.kt
@@ -0,0 +1,43 @@
+package com.example.instagram
+
+import com.parse.ParseClassName
+import com.parse.ParseFile
+import com.parse.ParseObject
+import com.parse.ParseUser
+
+//Description : String
+//Image: File
+//User: User
+@ParseClassName(value = "Post")
+class Post: ParseObject() {
+
+    fun getDescription(): String? {
+        return getString(KEY_DESCRIPTION)
+    }
+
+    fun setDescription(description: String){
+        put(KEY_DESCRIPTION, description)
+    }
+
+    fun getImage(): ParseFile? {
+        return getParseFile(KEY_IMAGE)
+    }
+
+    fun setImage(parseFile: ParseFile){
+        put(KEY_IMAGE, parseFile)
+    }
+
+    fun getUSer(): ParseUser?{
+        return getParseUser(KEY_USER)
+    }
+
+    fun setUser(user: ParseUser){
+        put(KEY_USER,user)
+    }
+
+    companion object {
+        const val KEY_DESCRIPTION = "description"
+        const val KEY_IMAGE = "image"
+        const val KEY_USER = "user"
+    }
+}
